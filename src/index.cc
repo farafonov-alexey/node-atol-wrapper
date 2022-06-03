25d72a6be570563fde83637bdcbecbc3f41162ed
Ustinov Vladislav <vlad.qqw@gmail.com>
2022-06-03 08:03:02 +0200

    Fixed NODE_MODULE to NAN_MODULE_WORKER_ENABLED (#25)

--- src/index.cc
+++ src/index.cc
@@ -6,5 +6,9 @@ NAN_MODULE_INIT(InitModule) {
 }
 
 DISABLE_WCAST_FUNCTION_TYPE
-NODE_MODULE(node_atol_wrapper, InitModule);
+#if NODE_MAJOR_VERSION >= 10
+NAN_MODULE_WORKER_ENABLED(node_atol_wrapper, InitModule)
+#else
+NODE_MODULE(node_atol_wrapper, InitModule)
+#endif
 DISABLE_WCAST_FUNCTION_TYPE_END
