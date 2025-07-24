af37750523588468ebb747d41ee6c32e02d898a4
Aleksandr <94838926+YaSoVuH@users.noreply.github.com>
2025-07-24 16:34:41 +0300

    updateto101070

---
+++ src/linux-x86/fptr10_t.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+appname=`basename "$0" | sed s,\.sh$,,`
+
+dirname=`dirname "$0"`
+tmp="${dirname#?}"
+
+if [ "${dirname%$tmp}" != "/" ]; then
+	dirname="$PWD/$dirname"
+fi
+
+LD_LIBRARY_PATH="$dirname":${LD_LIBRARY_PATH}
+LD_LIBRARY_PATH="$dirname"/..:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
+export QT_PLUGIN_PATH="$dirname/../"
+export QT_QPA_FONTDIR="$dirname/fonts"
+
+"$dirname/$appname" "$@"
